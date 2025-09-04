8a624d3c0ce3d76f27bebeb3d4c9a203849655f3
PuneetAsija <144670551+PuneetAsija@users.noreply.github.com>
2025-09-04 11:43:11 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 import React from "react";
-import Hero from "./components/Hero.jsx"; // Adjust the path based on your folder structure
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"; // Importing routing components
+
+// Importing components
+import Home from "./components/Home.jsx";
+import Hero from "./components/Hero.jsx";
 import Header from "./components/Header.jsx";
 import Footer from "./components/Footer.jsx";
 import WhyChoose from "./components/WhyChooseUs.jsx";
@@ -10,16 +14,20 @@ import About from "./components/About.jsx";
 
 const App = () => {
   return (
-    <div>
-      <Header />
-      <Hero />
-      <About />
-      <WhyChoose />
-      <Adventure />
-      <Travelform />
-      <Timeline />
-      <Footer />
-    </div>
+    <Router basename="my-project">
+      <div>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/why-choose" element={<WhyChoose />} />
+          <Route path="/adventure" element={<Adventure />} />
+          <Route path="/travelform" element={<Travelform />} />
+          <Route path="/timeline" element={<Timeline />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 };
 
